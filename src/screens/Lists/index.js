c1b73c1c528d9a21880922fc3f920dde141e1bd5
steven20022 <jacobelam11@gmail.com>
2022-09-21 18:02:50 -0400

    Update index.js

--- src/screens/Lists/index.js
+++ src/screens/Lists/index.js
@@ -25,6 +25,53 @@ const ListsScreen = props => {
     ]
   );
 
+  useEffect(() => {
+    const listener = navigation.addListener('focus', () => {
+      // declare an empty array to store the results from the SELECT
+      let results = []
+      // declare a transaction that will execute the SELECT
+      shopperDB.transaction(txn => {
+        // execute SELECT
+        txn.executeSql(
+          `SELECT * FROM ${listsTableNames}`,
+          [],
+          // callback function to handle the results from the
+          // SELECT s
+          (_, res) => {
+            // get number of rows of data selected
+            let len = res.rows.length;
+            console.log('Length of lists ' + len);
+            // if more than one row was returned
+            if (len > 0) {
+              // loop through the rows
+              for (let i = 0; i < len; i++) {
+                // push a row of data at a time onto the
+                // results array
+                let item = res.rows.item(i)
+                results.push({
+                  id: item.id,
+                  name: item.name,
+                  store: item.store,
+                  date: item.date
+                })
+              }
+              // assign results array to lists state variable
+              setLists(results);
+            } else {
+              // if no rows of data were returned
+              // set lists state variable to an empty array
+              setLists([])
+            }
+          },
+          error => {
+            console.log('Error getting listst ' + error.message);
+          },
+        )
+      });
+    });
+    return listener;
+  });
+
   return (
     <View style={styles.container}>
       <View>
